84c82994c8170e1c11f1563ffa6766ad7ad806a2
WonHeeJae <dnjsstgml@naver.com>
2024-10-14 20:46:07 +0900

    회원가입
    signup.js
    패스워드 해시처리
    패스워드 확인과 패스워드칸 값이 같으면 회원가입 진행가능케

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,14 +1,41 @@
 const form = document.querySelector("#signup-form");
+
+const checkPassword = () => {
+  const formData = new FormData(form);
+  const password1 = formData.get("password");
+  const password2 = formData.get("password2");
+  if (password1 === password2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
   const sha256Password = sha256(formData.get("password")); //해시보안처리
-
   formData.set("password", sha256Password);
-  console.log(formData.get("password"));
-  const res = await fetch("/signup", {
-    method: "POST",
-    body: formData,
-  });
+
+  const div = document.querySelector("#info");
+
+  if (checkPassword()) {
+    const res = await fetch("/signup", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    if (data === "200") {
+      div.innerText = "회원가입에 성공했습니다.";
+      div.style.color = "black";
+    }
+  } else {
+    div.innerText = "비밀번호가 같지 않습니다.";
+    div.style.color = "red";
+    /*const div = document.createElement("div");
+    div.innerText = "비밀번호가 다릅니다.";
+    div.style.color = "red";
+    form.appendChild(div);*/
+  }
 };
 form.addEventListener("submit", handleSubmit);
